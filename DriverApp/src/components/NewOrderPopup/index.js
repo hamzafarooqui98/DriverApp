81265b3bad71cc89b9b35dd265171b74946cee80
Hamza Farooqui <hamzattking@gmail.com>
2021-08-10 23:15:33 +0500

    DriverApp

--- DriverApp/src/components/NewOrderPopup/index.js
+++ DriverApp/src/components/NewOrderPopup/index.js
@@ -1,11 +1,10 @@
-import React from "react";
-import { View, Text, Pressable } from "react-native";
-import AntDesign from "react-native-vector-icons/AntDesign";
-import FontAwesome from "react-native-vector-icons/FontAwesome";
+import React from 'react';
+import {View, Text, Pressable} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import styles from './styles.js';
 
-const NewOrderPopup = ({ newOrder, onAccept, onDecline, duration, distance }) => {
-
+const NewOrderPopup = ({newOrder, onAccept, onDecline, duration, distance}) => {
   return (
     <View style={styles.root}>
       <Pressable onPress={onDecline} style={styles.declineButton}>
@@ -13,23 +12,21 @@ const NewOrderPopup = ({ newOrder, onAccept, onDecline, duration, distance }) =>
       </Pressable>
 
       <Pressable onPress={onAccept} style={styles.popupContainer}>
-
         <View style={styles.row}>
-          <Text style={styles.uberType}>{newOrder.type}</Text>
+          <Text style={styles.uberType}>UberX</Text>
 
           <View style={styles.userBg}>
-            <FontAwesome name={"user"} color={"white"} size={35} />
+            <FontAwesome name={'user'} color={'white'} size={35} />
           </View>
 
           <Text style={styles.uberType}>
-            <AntDesign name={"star"} size={18}/>
-            {newOrder.user.rating}
+            <AntDesign name={'star'} size={18} />
+            5.0
           </Text>
         </View>
 
         <Text style={styles.minutes}>{duration} min</Text>
         <Text style={styles.distance}>{distance} mi</Text>
-
       </Pressable>
     </View>
   );
